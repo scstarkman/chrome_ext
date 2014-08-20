4cd0309f2527bb241e81252e319bfa53ebc6b057
Susie Starkman <suzostudio@gmail.com>
2014-08-20 08:48:17 -0400

    initial commit

---
+++ app/scripts/popup.js
@@ -0,0 +1,28 @@
+'use strict';
+
+console.log('\'Allo \'Allo! Popup');
+
+$.ajax({
+    url: 'http://www.reddit.com/r/aww.json',
+    type: 'GET',
+    success:function(data) {
+
+      console.log(data);
+      console.log(data.data.children[2].data.title);
+
+      var html = '<div>';
+
+      for (var i=0; i< 10; i++){
+        html += '<img src = ' + data.data.children[i].data.thumbnail + '>';
+        '<p>' + data.data.children[i].data.title + '</p>';
+      }
+
+      html += '</div>';
+
+      $('.putstuff').html(html);
+
+
+    }//end function
+
+
+}); //end .ajax
